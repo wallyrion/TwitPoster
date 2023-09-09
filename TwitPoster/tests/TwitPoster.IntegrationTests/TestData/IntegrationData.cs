b88542eff2822793113d8381e94fe60a3a3afec7
Oleksii Korniienko <oleksii.korniienko@idealscorp.com>
2023-09-09 23:44:20 +0300

    Refactor integration tests setup to use IntegrationTestWebFactory
    Created a shared `IntegrationTestWebFactory` for all tests to set up required configurations, improving the DRY principle. Test cases now are using `ApiClient` which is configured in `IntegrationTestWebFactory`, instead of a local `HttpClient`. New

---
+++ TwitPoster/tests/TwitPoster.IntegrationTests/TestData/IntegrationData.cs
@@ -0,0 +1,50 @@
+﻿using AutoFixture;
+using Microsoft.EntityFrameworkCore;
+using TwitPoster.DAL;
+using TwitPoster.DAL.Models;
+
+namespace TwitPoster.IntegrationTests.TestData;
+
+public class IntegrationData
+{
+    public Fixture BaseFixture { get; } = new();
+
+    private readonly TwitPosterContext _dbContext;
+    
+    public void Initialize(int defaultUserId)
+    {
+        BaseFixture.Customize<DAL.Models.Post>(x => x
+            .With(p => p.AuthorId, defaultUserId)
+            .Without(p => p.Id)
+            .Without(p => p.Author)
+            .Without(p => p.Comments)
+            .Without(p => p.PostLikes));
+    }
+    
+    public IntegrationData(TwitPosterContext dbContext)
+    {
+        _dbContext = dbContext;
+        BaseFixture.Behaviors.OfType<ThrowingRecursionBehavior>().ToList()
+            .ForEach(b => BaseFixture.Behaviors.Remove(b));
+        BaseFixture.Behaviors.Add(new OmitOnRecursionBehavior());
+        
+        BaseFixture.Customize<User>(x => x.Without(u => u.Id));
+        BaseFixture.Customize<UserAccount>(x => x.Without(u => u.Id).Without(u => u.IsBanned));
+    }
+    
+    public async Task<IReadOnlyList<T>> AddMany<T>(int count = 3) where T : class
+    {
+        var entities = BaseFixture.CreateMany<T>(count).ToList();
+        _dbContext.Set<T>().AddRange(entities);
+        await _dbContext.SaveChangesAsync();
+
+        return entities;
+    }
+    
+    public async Task<IReadOnlyList<T>> AddMany<T>(IReadOnlyList<T> entitiesToAdd) where T : class
+    {
+        _dbContext.Set<T>().AddRange(entitiesToAdd);
+        await _dbContext.SaveChangesAsync();
+        return entitiesToAdd;
+    }
+}
