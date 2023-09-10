57e3e4a4cd0b9d494cee32a290e981351bcabc9b
Oleksii Korniienko <oleksii.korniienko@idealscorp.com>
2023-09-10 20:08:18 +0300

    Remove MimeKit dependency and add TextFormat enum
    The MimeKit package reference in TwitPoster.Shared.csproj file was removed as it was unnecessary and replaced with an in-built TextFormat enum in the EmailCommand.cs file. This was

--- TwitPoster.Shared/TwitPoster.Shared/Contracts/EmailCommand.cs
+++ TwitPoster.Shared/TwitPoster.Shared/Contracts/EmailCommand.cs
@@ -1,7 +1,22 @@
-﻿using MimeKit.Text;
+﻿namespace TwitPoster.Shared.Contracts;
 
-namespace TwitPoster.Shared.Contracts;
-
+public enum TextFormat
+{
+    /// <summary>The plain text format.</summary>
+    Plain = 0,
+    /// <summary>An alias for the plain text format.</summary>
+    Text = 0,
+    /// <summary>The flowed text format (as described in rfc3676).</summary>
+    Flowed = 1,
+    /// <summary>The HTML text format.</summary>
+    Html = 2,
+    /// <summary>The enriched text format.</summary>
+    Enriched = 3,
+    /// <summary>The compressed rich text format.</summary>
+    CompressedRichText = 4,
+    /// <summary>The rich text format.</summary>
+    RichText = 5,
+}
 public sealed class EmailCommand
 {
     public string To { get; set; }
