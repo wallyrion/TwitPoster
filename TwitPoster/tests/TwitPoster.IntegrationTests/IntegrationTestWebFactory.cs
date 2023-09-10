bc007ca4aa9d459f10d20a96fe0385ed34c600c3
Oleksii Korniienko <oleksii.korniienko@idealscorp.com>
2023-09-11 01:38:36 +0300

    Add MassTransit test harness to integration tests
    An extra line of code was added to `IntegrationTestWebFactory.cs` in the `TwitPoster.IntegrationTests` testing module. The purpose of the update was to include the use of MassTransit Test Harness for more robust integration testing.

--- TwitPoster/tests/TwitPoster.IntegrationTests/IntegrationTestWebFactory.cs
+++ TwitPoster/tests/TwitPoster.IntegrationTests/IntegrationTestWebFactory.cs
@@ -1,4 +1,5 @@
 ﻿using System.Data.Common;
+using MassTransit;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Data.SqlClient;
@@ -32,6 +33,7 @@ public class IntegrationTestWebFactory : WebApplicationFactory<IApiTestMarker>, 
             }
 
             services.AddDbContext<TwitPosterContext>(options => options.UseSqlServer(_msSqlContainer.GetConnectionString()));
+            services.AddMassTransitTestHarness();
         });
     }
     
