c5081537818cc2d0eedcae845d1b603b7d5234c6
Oleksii Korniienko <oleksii.korniienko@idealscorp.com>
2023-09-13 22:42:56 +0300

    Switch messaging service to Azure Service Bus
    The messaging service is switched from local RabbitMQ service to Azure Service Bus. This is done to unify communication between applications and to make use of Azure’s powerful services for production environments. Changes made include the addition of Azure Bus configuration and removal of RabbitMQ configuration. Additionally, MailOptions, IServiceBusMessageScheduler and EmailCommandConsumer are added to accommodate for these changes.

--- TwitPoster.EmailSender/src/TwitPoster.EmailSender/Program.cs
+++ TwitPoster.EmailSender/src/TwitPoster.EmailSender/Program.cs
@@ -2,7 +2,9 @@ using System.Reflection;
 using MassTransit;
 using Serilog;
 using TwitPoster.EmailSender;
+using TwitPoster.EmailSender.Consumer;
 using TwitPoster.EmailSender.Services;
+using TwitPoster.Shared.Contracts;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Host.UseSerilog((ctx, lc) => lc
@@ -14,7 +16,7 @@ var mailOptions = builder.Configuration.GetRequiredSection("Mail");
 
 builder.Services
     .Configure<MailOptions>(mailOptions)
-    .Configure<RabbitMqTransportOptions>(rabbitMqConfig)
+    //.Configure<RabbitMqTransportOptions>(rabbitMqConfig)
     .AddScoped<IEmailService, EmailService>()
     
     .AddMassTransit(x =>
@@ -22,19 +24,21 @@ builder.Services
         var entryAssembly = Assembly.GetExecutingAssembly();
         x.AddConsumers(entryAssembly);
 
-        if (builder.Environment.IsDevelopment())
+        /*if (builder.Environment.IsDevelopment())
         {
             x.UsingRabbitMq((context, cfg) => cfg.ConfigureEndpoints(context));
-        }
-        else
-        {
-            x.UsingAzureServiceBus((context, cfg) =>
-            {
-                cfg.Host(builder.Configuration.GetConnectionString("ServiceBus"));
+        }{*/
         
-                cfg.ConfigureEndpoints(context);
-            });    
-        }
+        x.UsingAzureServiceBus((context, cfg) =>
+        {
+            cfg.Host(builder.Configuration.GetConnectionString("ServiceBus"));
+            
+            cfg.UseServiceBusMessageScheduler();
+            cfg.SubscriptionEndpoint<EmailCommand>($"EmailSenderSubscription" , configurator =>
+            {
+                configurator.ConfigureConsumer<EmailCommandConsumer>(context);
+            });
+        });      
     });
 
 var app = builder.Build();
