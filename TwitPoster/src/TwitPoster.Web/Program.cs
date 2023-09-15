dd4ef5a67b9d0cd1182dfd0facbf50b6c5babe82
Oleksii Korniienko <oleksii.korniienko@idealscorp.com>
2023-09-15 20:23:17 +0300

    Add additional health check endpoints
    Two new http GET endpoints '/healthy' and

--- TwitPoster/src/TwitPoster.Web/Program.cs
+++ TwitPoster/src/TwitPoster.Web/Program.cs
@@ -82,6 +82,8 @@ builder.Services.AddHttpClient<ILocationClient, LocationClient>(client
 var app = builder.Build();
 
 app.MapGet("/health", () => "OK");
+app.MapGet("/healthy", () => "OK");
+app.MapGet("/healthy2", () => "OK");
 
 app.MapControllers()
     .RequireAuthorization();
