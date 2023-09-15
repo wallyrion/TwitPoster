8c62231e96186c667371c03e2203dce302ae7370
Oleksii Korniienko <oleksii.korniienko@idealscorp.com>
2023-09-15 20:08:25 +0300

    Remove duplicate health check endpoint
    The "/healthy" endpoint in Program.cs was removed because it was duplicating the functionality of the "/

--- TwitPoster/src/TwitPoster.Web/Program.cs
+++ TwitPoster/src/TwitPoster.Web/Program.cs
@@ -82,7 +82,6 @@ builder.Services.AddHttpClient<ILocationClient, LocationClient>(client
 var app = builder.Build();
 
 app.MapGet("/health", () => "OK");
-app.MapGet("/healthy", () => "OK");
 
 app.MapControllers()
     .RequireAuthorization();
