331aa1dd18ccdb38993f3417edc1baa1cbb43c04
Oleksii Korniienko <oleksii.korniienko@idealscorp.com>
2023-09-19 23:09:02 +0300

    Add /health2 endpoint to web application
    Added a new health endpoint ("/health2") to the web application for better

--- TwitPoster/src/TwitPoster.Web/Program.cs
+++ TwitPoster/src/TwitPoster.Web/Program.cs
@@ -82,6 +82,7 @@ builder.Services.AddHttpClient<ILocationClient, LocationClient>(client
 var app = builder.Build();
 
 app.MapGet("/health", () => "OK");
+app.MapGet("/health2", () => "OK");
 
 
 app.MapControllers()
