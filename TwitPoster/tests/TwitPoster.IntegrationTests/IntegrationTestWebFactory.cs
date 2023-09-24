be464413cc1bb26e9219cffda245cc8dc53219c5
Oleksii Korniienko <oleksii.korniienko@idealscorp.com>
2023-09-25 00:23:37 +0300

    Add authentication secret to IntegrationTestWebFactory
    The "Auth:Secret" key-value pair has been added to the InMemoryCollection in

--- TwitPoster/tests/TwitPoster.IntegrationTests/IntegrationTestWebFactory.cs
+++ TwitPoster/tests/TwitPoster.IntegrationTests/IntegrationTestWebFactory.cs
@@ -37,6 +37,7 @@ public class IntegrationTestWebFactory : WebApplicationFactory<IApiTestMarker>, 
                 KeyValuePair.Create("ConnectionStrings:Redis", _redisContainer.GetConnectionString()),
                 KeyValuePair.Create("ConnectionStrings:DbConnection", _msSqlContainer.GetConnectionString()),
                 KeyValuePair.Create("Secrets:UseSecrets", "false"),
+                KeyValuePair.Create("Auth:Secret", "mysecret"),
             };
             x.AddInMemoryCollection(collection!);
         });
