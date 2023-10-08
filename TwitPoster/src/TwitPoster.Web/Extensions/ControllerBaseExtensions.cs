d74da1ee94e2f58865e47a57e6a5dbd041c8c8fb
Oleksii Korniienko <oleksii.korniienko@idealscorp.com>
2023-10-08 14:52:32 +0300

    Add tests for confirm email

--- TwitPoster/src/TwitPoster.Web/Extensions/ControllerBaseExtensions.cs
+++ TwitPoster/src/TwitPoster.Web/Extensions/ControllerBaseExtensions.cs
@@ -5,13 +5,6 @@ namespace TwitPoster.Web.Extensions;
 
 public static class ControllerBaseExtensions
 {
-    public static ActionResult ToOk<T, TResponse>(this ControllerBase controller, Result<T> result, Func<T, TResponse> successMap)
-    {
-        return result.Match(
-            successObject => controller.Ok(successMap(successObject)),
-            exception => controller.Problem(statusCode: StatusCodes.Status400BadRequest, title: exception.Message));
-    }
-
     public static ActionResult ToActionResult<T, TResponse>(this ControllerBase controller, Result<T> result, Func<T, TResponse> successMap)
         where TResponse : ActionResult
     {
