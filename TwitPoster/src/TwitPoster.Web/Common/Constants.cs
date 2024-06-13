86a34416b27131197abd8df6f9e467714e254f26
wallyrion <30635131+wallyrion@users.noreply.github.com>
2024-06-13 18:57:19 +0300

    Modified upload photo endpoint (#75)

--- TwitPoster/src/TwitPoster.Web/Common/Constants.cs
+++ TwitPoster/src/TwitPoster.Web/Common/Constants.cs
@@ -1,7 +1,16 @@
-﻿namespace TwitPoster.Web.Common;
+﻿using System.Collections.Frozen;
+
+namespace TwitPoster.Web.Common;
 
 internal static class WebConstants
 {
+    public static readonly FrozenSet<string> ValidProfileImageExtensions = new HashSet<string>(
+    [
+        ".jpg",
+        ".jpeg",
+        ".bmp"
+    ]).ToFrozenSet();
+
     internal static class Cors
     {
         public const string DefaultPolicy = "DefaultPolicy";
