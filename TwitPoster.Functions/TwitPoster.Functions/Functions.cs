3544860127384c8a1c50284f7bab77091802cda6
Oleksii Korniienko <kornienko1296@gmail.com>
2024-06-15 12:58:28 +0300

    a

--- TwitPoster.Functions/TwitPoster.Functions/Functions.cs
+++ TwitPoster.Functions/TwitPoster.Functions/Functions.cs
@@ -12,18 +12,30 @@ public class Functions(ILoggerFactory loggerFactory, BlobServiceClient blobServi
     [Function("CompressImageFunction")]
     public async Task Run([BlobTrigger("twitposter/user/{userId}/images/profile/main/{name}", Connection = "AzureWebJobsStorage")] Stream blob, int userId, string name)
     {
+        try
+        {
+            using var newStream = new MemoryStream();
+            await ResizeImage0(blob, newStream);
+
+            var extension = Path.GetExtension(name);
+
+            var thumbnailBlobClient = blobServiceClient.GetBlobContainerClient("twitposter-local").GetBlobClient($"user/{userId}/images/profile/thumbnail/image{extension}");
+
+            newStream.Position = 0;
+            await thumbnailBlobClient.UploadAsync(newStream, overwrite: true);
+            _logger.LogInformation("Thumbnail for {UserId} created and uploaded", userId);
+        }
+        catch (Exception e)
+        {
+            
+            
+            _logger.LogError(e, "Error while processing CompressImageFunction");
+            Console.WriteLine(e);
+            throw;
+        }
         // Upload the thumbnail to the "profile-thumbnails" container
 
-        using var newStream = new MemoryStream();
-        await ResizeImage0(blob, newStream);
-
-        var extension = Path.GetExtension(name);
-
-        var thumbnailBlobClient = blobServiceClient.GetBlobContainerClient("twitposter-local").GetBlobClient($"user/{userId}/images/profile/thumbnail/image{extension}");
-
-        newStream.Position = 0;
-        await thumbnailBlobClient.UploadAsync(newStream, overwrite: true);
-        _logger.LogInformation("Thumbnail for {UserId} created and uploaded", userId);
+        
     }
 
     private async Task ResizeImage0(Stream stream, Stream blob)
