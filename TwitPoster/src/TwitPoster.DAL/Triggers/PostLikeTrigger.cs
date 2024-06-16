c24d797b974c4d94ee7785afaf7f74c503c834d1
wallyrion <30635131+wallyrion@users.noreply.github.com>
2024-06-16 21:48:25 +0300

    Fix PostLike (#83)

--- TwitPoster/src/TwitPoster.DAL/Triggers/PostLikeTrigger.cs
+++ TwitPoster/src/TwitPoster.DAL/Triggers/PostLikeTrigger.cs
@@ -13,7 +13,9 @@ public sealed class PostLikeTrigger(TwitPosterContext dbContext) : IAfterSaveTri
             return;
         }
 
-        await dbContext.Posts.ExecuteUpdateAsync(
+        await dbContext.Posts
+            .Where(p => p.Id == context.Entity.PostId)
+            .ExecuteUpdateAsync(
             p => p.SetProperty(post => post.LikesCount, 
                 post => context.ChangeType == ChangeType.Added ? post.LikesCount + 1 : post.LikesCount - 1),
             cancellationToken: cancellationToken);
