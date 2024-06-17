6ab654e20894ca09b746642f1a44729b519e2b50
Oleksii Korniienko <kornienko1296@gmail.com>
2024-06-18 01:03:47 +0300

    Refactoring Functions a bit

--- TwitPoster.Functions/TwitPoster.Functions/StorageOptions.cs
+++
@@ -1,8 +0,0 @@
-namespace TwitPoster.Functions;
-
-public class StorageOptions
-{
-    public string Uri { get; set; } = null!;
-    public string AccountName { get; set; } = null!;
-    public string SharedKey { get; set; } = null!;
-}
