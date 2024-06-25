7d7a3a3031f120828426dc5ef7194c5a55b8b9cb
Oleksii Korniienko <kornienko1296@gmail.com>
2024-06-26 00:43:29 +0300

    added function app

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,5 @@
-﻿terraform {
+﻿
+terraform {
   backend "azurerm" {
     resource_group_name   = "tfstate-rg"
     storage_account_name  = "tfstate12345"
@@ -53,10 +54,6 @@ resource "azurerm_mssql_server" "sqlserver" {
   administrator_login          = "sqladmin"
   administrator_login_password = "P@ssw0rd1234!"
   minimum_tls_version          = "1.2"
-
-  tags = {
-    environment = "production"
-  }
 }
 
 resource "azurerm_mssql_database" "sqldatabase" {
@@ -65,6 +62,11 @@ resource "azurerm_mssql_database" "sqldatabase" {
   sku_name  = "S0"
 }
 
+locals {
+  sql_connection_string = <<EOF
+Server=tcp:${azurerm_mssql_server.sqlserver.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.sqldatabase.name};Persist Security Info=False;User ID=${azurerm_mssql_server.sqlserver.administrator_login};Password=${azurerm_mssql_server.sqlserver.administrator_login_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;
+EOF
+}
 
 resource "azurerm_service_plan" "asp" {
   name                = "twitposter-${var.environment}-asp"
@@ -74,10 +76,48 @@ resource "azurerm_service_plan" "asp" {
   sku_name            = "F1"
 }
 
-locals {
-  sql_connection_string = <<EOF
-Server=tcp:${azurerm_mssql_server.sqlserver.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.sqldatabase.name};Persist Security Info=False;User ID=${azurerm_mssql_server.sqlserver.administrator_login};Password=${azurerm_mssql_server.sqlserver.administrator_login_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;
-EOF
+
+
+resource "azurerm_service_plan" "function_app_plan" {
+  name                = "twitposter-${var.environment}-function-app-plan"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  os_type             = "Linux"
+  sku_name            = "Y1"
+}
+
+# Define Application Insights
+resource "azurerm_application_insights" "function_app_insights" {
+  name                = "functionapp-appinsights-${var.environment}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  application_type    = "web"
+}
+
+resource "azurerm_linux_function_app" "functionapp" {
+  name                       = "twitposter-${var.environment}-functionapp"
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  service_plan_id           = azurerm_service_plan.function_app_plan.id
+  storage_account_name       = azurerm_storage_account.storage.name
+  storage_account_access_key = azurerm_storage_account.storage.primary_access_key
+
+  site_config {
+    application_stack {
+      dotnet_version = "8.0"
+      use_dotnet_isolated_runtime = true
+    }
+  }
+
+  app_settings = {
+    "AzureWebJobsStorage"               = "DefaultEndpointsProtocol=https;AccountName=${azurerm_storage_account.storage.name};AccountKey=${azurerm_storage_account.storage.primary_access_key};EndpointSuffix=core.windows.net"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  https_only = true
 }
 
 resource "azurerm_linux_web_app" "appservice" {
@@ -156,4 +196,9 @@ output "emailsender_app_service_default_hostname" {
 output "emailsender_publish_profile_command" {
   value       = "az webapp deployment list-publishing-profiles --name ${azurerm_linux_web_app.emailsender.name} --resource-group ${azurerm_resource_group.rg.name} --xml"
   description = "Run this command in your shell to retrieve the Azure Email Sender Web App's publishing profile."
+}
+
+output "function_app_publish_profile_command" {
+  value       = "az functionapp deployment list-publishing-profiles --name ${azurerm_linux_function_app.functionapp.name} --resource-group ${azurerm_resource_group.rg.name} --xml"
+  description = "Run this command in your shell to retrieve the Azure Function App's publishing profile."
 }
