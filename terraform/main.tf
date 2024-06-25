8b076818a7678ff946cf55072ebed9236e88d676
Oleksii Korniienko <kornienko1296@gmail.com>
2024-06-25 16:17:12 +0300

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,13 @@
-﻿provider "azurerm" {
+﻿terraform {
+  backend "azurerm" {
+    resource_group_name   = "tfstate-rg"
+    storage_account_name  = "tfstate12345"
+    container_name        = "tfstate"
+    key                   = "terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
   features {}
 }
 
