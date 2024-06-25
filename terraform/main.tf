c1af1d4d6197935d58e4debc6643a9a1ac98ecad
Oleksii Korniienko <kornienko1296@gmail.com>
2024-06-25 15:31:58 +0300

    publish profile local data

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,21 +37,6 @@ resource "azurerm_linux_web_app" "appservice" {
   https_only              = true
 }
 
-resource "null_resource" "fetch_publish_profile" {
-  depends_on = [azurerm_linux_web_app.appservice]
-
-  provisioner "local-exec" {
-    command = <<EOT
-      az webapp deployment list-publishing-profiles --name ${azurerm_linux_web_app.appservice.name} --resource-group ${azurerm_resource_group.rg.name} --xml > .terraform/publish_profile.xml
-    EOT
-  }
-}
-
-data "local_file" "publish_profile" {
-  depends_on = [null_resource.fetch_publish_profile]
-  filename   = "${path.module}/.terraform/publish_profile.xml"
-}
-
 output "app_service_name" {
   value = azurerm_linux_web_app.appservice.name
 }
