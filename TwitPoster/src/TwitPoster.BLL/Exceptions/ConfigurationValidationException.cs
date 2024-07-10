7c98cb427b330db0e7b318039043fe3c1e9a57cc
wallyrion <30635131+wallyrion@users.noreply.github.com>
2024-07-10 10:22:12 +0300

    Group ef package update (#165)

--- TwitPoster/src/TwitPoster.BLL/Exceptions/ConfigurationValidationException.cs
+++ TwitPoster/src/TwitPoster.BLL/Exceptions/ConfigurationValidationException.cs
@@ -1,4 +1,3 @@
 namespace TwitPoster.BLL.Exceptions;
 
-[Serializable]
 public class ConfigurationValidationException(string configType, Exception ex) : Exception($"Configuration type '{configType}' validation failed", ex);
