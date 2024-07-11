6e182e38a0d63f86052f4de6895d6f304ea03652
Oleksii Korniienko <kornienko1296@gmail.com>
2024-07-11 13:56:29 +0300

    1

--- TwitPoster.Chat/docker/scripts/init-mongos.js
+++ TwitPoster.Chat/docker/scripts/init-mongos.js
@@ -1,4 +1,6 @@
+print("Initializing Mongos Router...");
 sh.addShard("shard1ReplSet/shard1:27018");
 sh.addShard("shard2ReplSet/shard2:27028");
 sh.enableSharding("ChatDb");
 sh.shardCollection("ChatDb.Messages", { chatRoomId: 1 });
+print("Mongos Router Initialized.");
--- TwitPoster.Chat/docker/scripts/init-shard1.js
+++ TwitPoster.Chat/docker/scripts/init-shard1.js
@@ -1,4 +1,6 @@
+print("Initializing Shard1...");
 rs.initiate({
     _id: "shard1ReplSet",
     members: [{ _id: 0, host: "shard1:27018" }]
 });
+print("Shard1 Initialized.");
