e84c8a23d4a3c9bc002b9f9a0a53c9faad26cadc
Oleksii Korniienko <30635131+wallyrion@users.noreply.github.com>
2025-01-09 19:39:03 +0100

    [Functions] Add test log to console (#438)

--- TwitPoster.Functions/TwitPoster.Functions/Program.cs
+++ TwitPoster.Functions/TwitPoster.Functions/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
+Console.WriteLine("Initializing function builder");
 
 var builder = FunctionsApplication.CreateBuilder(args);
 
