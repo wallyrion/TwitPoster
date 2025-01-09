671348c86e67411510e5a57940326d747cd34849
Oleksii Korniienko <30635131+wallyrion@users.noreply.github.com>
2025-01-09 09:56:58 +0100

    [Functions] Deploy test (#432)

--- TwitPoster.Functions/TwitPoster.Functions/Program.cs
+++ TwitPoster.Functions/TwitPoster.Functions/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
+
 var host = new HostBuilder()
     .ConfigureFunctionsWebApplication()
     .ConfigureServices((context, services) =>
