fe53715e4c60c88e1e7d77cb4354fbb51f1aa795
Oleksii Korniienko <kornienko1296@gmail.com>
2025-01-09 20:41:18 +0100

    1

---
+++ Twitposter.TestFunctions/TwitPoster.Functions/Program.cs
@@ -0,0 +1,39 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Extensions.Azure;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+Console.WriteLine("Initializing function builder");
+
+var builder = FunctionsApplication.CreateBuilder(args);
+
+builder.ConfigureFunctionsWebApplication();
+
+var storageConnectionString = builder.Configuration.GetValue<string>("AzureWebJobsStorage");
+
+
+// Application Insights isn't enabled by default. See https://aka.ms/AAt8mw4.
+builder.Services
+    .AddApplicationInsightsTelemetryWorkerService()
+    .ConfigureFunctionsApplicationInsights();
+
+builder.Services.AddAzureClients(clientBuilder => { clientBuilder.AddBlobServiceClient(storageConnectionString); });
+
+
+builder.Build().Run();
+
+
+/*var host = new HostBuilder()
+    .ConfigureFunctionsWebApplication()
+    .ConfigureServices((context, services) =>
+    {
+        services.AddApplicationInsightsTelemetryWorkerService();
+        services.ConfigureFunctionsApplicationInsights();
+
+        var storageConnectionString = context.Configuration.GetValue<string>("AzureWebJobsStorage");
+
+    })
+    .Build();*/
+
